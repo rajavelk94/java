3720df8bd7e8583ff222f627517ae57397034d09
Rajavelkandasamy <108261403+Rajavelkandasamy@users.noreply.github.com>
2022-08-19 21:55:00 +0530

    ok

---
+++ datatypes/Test.java
@@ -0,0 +1,39 @@
+/* class Test{
+	public static void main(String[] args){
+	
+		boolean flag  = true; //valid
+		boolean avail = True; //CE
+		boolean a = 1;        //CE
+		boolean b = 0;        //CE
+		System.out.println(flag);
+		System.out.println(avail);
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
+ */
+/* class Test{
+	public static void main(String[] args){
+	
+		byte b = 1000;              
+		System.out.println(b);
+	}
+}
+ */
+
+class Test{
+	public static void main(String[] args){
+	
+		int a = 10;
+		int b = 010;
+		int c = 0x10;
+		float rating  = 2.8f;
+		double rating1 = 2.8;
+		System.out.println(rating);
+		System.out.println(rating1);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
